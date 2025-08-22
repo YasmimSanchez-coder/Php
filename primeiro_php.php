743e2be7c0655243d1a33d0d659d09e163829db1
Yasmim Sanchez <yasmim.ouvinhas@fatec.sp.gov.br>
2025-08-22 10:36:48 -0300

    Add files via upload

---
+++ primeiro_php.php
@@ -0,0 +1,21 @@
+<?php
+	$nome = "Yasmim Sanchez Ouvinhas";
+	$idade = 19;
+	$celular = "(14)991711771";
+	$altura = 1.50;
+	$namorando = true;
+	
+	echo "<h3 style='color:blue'>O meu nome é $nome e tenho $idade anos</h3><br>";
+	echo "Tenho de altura $altura e meu celular é $celular<br>";
+	//verifica se namorando é verdadeiro
+	if($namorando)
+	{
+		echo "tenho namorada";
+	}
+	else
+	{
+		echo "NÃO Sou solteira";
+	}
+	
+	
+?>
